a2daac5e2b31e754d861299ee2152bd81ae6fdf1
mft <70207320+PLCwithoutP@users.noreply.github.com>
2025-07-16 16:31:30 +0000

    2_ThermalBath

---
+++ su2/2_N2-N_thermalNonEq/purgeTimer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true; do
+  /home/t12022124597/Desktop/cfdProjects/su2/thermalBath/2_N2-N_thermalNonEq/patternPurgeWrite.sh
+  echo "Purging round is completed!"
+  sleep 60
+done
